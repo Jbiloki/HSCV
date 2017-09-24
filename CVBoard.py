451fffe43a0b53f4bd491578c6c289a70e0d8f60
Jacob <Bilokij@gmail.com>
2017-09-23 21:24:50 -0700

    file clean up

--- CVBoard.py
+++ CVBoard.py
@@ -52,7 +52,6 @@ for(i,c) in enumerate(refCnts):
     cv2.rectangle(orig, (x,y),(x+w,y+h),(0,0,255),2)
     roi = ref[y:y+h+ 1, x:x+w]
     roi = cv2.resize(roi, (57,88))
-    bef = roi
     roi = rank.equalize(roi, disk(10))
     digits[i] = roi
           
@@ -113,29 +112,32 @@ for(i, (gX,gY,gW,gH)) in enumerate(locs):
         if(w > 3 and w < 20) and (h > 5 and h < 20):
             
             cv2.rectangle(o, (x,y),(x+w,y+h),(0,0,255),1)
-            roi = group[y :y+ h, x:x + w ]
-            non_rotated[idx] = cv2.resize(roi, (57,88))
-            coords = np.column_stack(np.where(roi > 0))
-            angle = cv2.minAreaRect(coords)[-1]
-            if(angle < -45):
-                angle = -(90 + angle)
-            else:
-                angle = -angle
-            (h , w) = roi.shape[:2]
-            center = (w//2, h//2)
-            M = cv2.getRotationMatrix2D(center, angle, 1.0)
-            rotated = cv2.warpAffine(roi, M, (w, h), flags = cv2.INTER_CUBIC, borderMode = cv2.BORDER_REPLICATE)
-            cv2.rectangle(o, (x,y),(x+w,y+h),(0,0,255),1)
-            roi = cv2.resize(rotated, (57,88))#(57,88))
-            crops[idx] = roi
-            idx += 1
-            scores = []
-            for(digit, digitROI) in digits.items():
-                result = cv2.matchTemplate(roi, digitROI, cv2.TM_CCOEFF)
-                #(_, score, _ , _) = cv2.minMaxLoc(result)
-                scores.append(result)
-            allout.append(scores)
-            groupOutput.append(str(np.argmax(scores)))       
+            roi = group[y :y+ h, x:x + w]
+            if not roi is None:
+                non_rotated[idx] = cv2.resize(roi, (57,88))
+                coords = np.column_stack(np.where(roi > 0))
+                angle = cv2.minAreaRect(coords)[-1]
+                if(angle < -45):
+                    angle = -(90 + angle)
+                else:
+                    angle = -angle
+                (h , w) = roi.shape[:2]
+                center = (w//2, h//2)
+                M = cv2.getRotationMatrix2D(center, angle, 1.0)
+                rotated = cv2.warpAffine(roi, M, (w, h), flags = cv2.INTER_CUBIC, borderMode = cv2.BORDER_REPLICATE)
+                bef = rotated
+                rotated = rank.equalize(rotated, disk(7))
+                cv2.rectangle(o, (x,y),(x+w,y+h),(0,0,255),1)
+                roi = cv2.resize(rotated, (57,88))#(57,88))
+                crops[idx] = roi
+                idx += 1
+                scores = []
+                for(digit, digitROI) in digits.items():
+                    result = cv2.matchTemplate(roi, digitROI, cv2.TM_CCOEFF)
+                    (_, score, _ , _) = cv2.minMaxLoc(result)
+                    scores.append(result)
+                allout.append(scores)
+                groupOutput.append(str(np.argmax(scores)))       
     
 
 #print(refCnts[0], refCnts[0].shape, refCnts[0][2])
@@ -143,13 +145,14 @@ for(i, (gX,gY,gW,gH)) in enumerate(locs):
 #print(np.array(counts).shape)
 #locs = np.array(locs).reshape((-1,1,2)).astype(np.int32)
 #print(cnts[28])
+print(allout[0])
 print(groupOutput)
 outDict = {0:'A',1:'a',2:'b',3:'B',4:'c',5:'C',6:'d',7:'D',8:'e',9:'E',10:'f',11:'F',12:'g',13:'G',14:'h',15:'H',16:'i',17:'j',18:'I',19:'k',20:'J',21:'l',22:'K',23:'m',
            24:'L',25:'n',26:'M',27:'o',28:'N',29:'p',30:'O',31:'q',32:'p',33:'r',34:'Q',35:'S',36:'t',37:'R',38:'u',39:'S',40:'v',41:'T',42:'w',43:'U',44:'x',45:'V',46:'y',47:'W',48:'z',49:'X',
            50:'Y',51:'Z'}
 #cv2.imshow("daf", o)
 cv2.imshow("asdf", o)
-
+cv2.imshow("true", crops[0])
 cv2.imshow("template", orig)
 cardName = ""
 for i in range(len(groupOutput)):
