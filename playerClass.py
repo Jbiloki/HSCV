f581878aac69ede69b5acf6fde64df12a0eec8cf
Jbiloki <bilokij@gmail.com>
2017-09-29 18:33:27 -0700

    laptop

--- playerClass.py
+++ playerClass.py
@@ -5,14 +5,28 @@ Created on Wed Sep 27 17:40:03 2017
 @author: Nguyen
 """
 
-class playerHand:
+class GameState:
     def __init__(self):
-        self.currHand = []
+        self.playerHand = []
+        self.rivalBoard = []
+        self.playerBoard = []
+        
+    #Player Methods
     def displayHand(self):
-        print(self.currHand)
+        print(self.playerHand)
     def addToHand(self, card):
         if card is not None:
-            self.currHand.append(card)
+            self.playerHand.append(card)
     def removeFromHand(self, card):
-        if card in currHand:
-            currHand.remove(card)
+        if card in self.playerHand:
+            self.playerHand.remove(card)
+    
+    #Game Board Methods
+    def displayBoard(self):
+        print("Rival: ",self.rivalBoard, "\n Your Board: ", self.playerBoard)
+    def addToRival(self, card):
+        if card is not None:
+            self.rivalBoard.append(card)
+    def addToPlayer(self, card):
+        if card is not None:
+            self.playerBoard.append(card)
--- video_board.py
+++ video_board.py
@@ -51,15 +51,15 @@ def process_img(im):
     return processed
     
 #Take region of roi containing a card name and pass it to CVBoard.py to describe card
-def findCards(orig,gray, player):
+def findCards(orig,gray, game):
     rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,3))
     sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
     orig = imutils.resize(orig, width = 300)
     if orig.any():
         cv2.bitwise_not(gray)
         tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
-        blur = cv2.GaussianBlur(tophat, (5,5),5)
-        gradX = cv2.Sobel(blur, ddepth=cv2.CV_32F, dx = 1, dy = 0, ksize = -1)
+        #blur = cv2.GaussianBlur(tophat, (5,5),5)
+        gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx = 1, dy = 0, ksize = 1)
         gradX = np.absolute(gradX)
         
         #Minmax normalize
@@ -69,7 +69,6 @@ def findCards(orig,gray, player):
         
         gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
         thresh = cv2.threshold(gradX, 10, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-        
         thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
         
         cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -82,7 +81,7 @@ def findCards(orig,gray, player):
                 cv2.rectangle(orig,(x,y), (x+w, y+h),(255,0,0),2)
                 locs.append((x - 5,y,w + 10,h + 5))
             if locs:
-                player.addToHand(CVBoard.addFromBoard(orig,locs,gray)) #Send roi region (should be the name) to be described
+                game.addToHand(CVBoard.addFromBoard(orig,locs,gray)) #Send roi region (should be the name) to be described
                 #time.sleep(1)
                 locs = []
 
@@ -96,20 +95,17 @@ def main():
         roi_board = cv2.cvtColor(np.array(roi_board), cv2.COLOR_BGR2RGB)
         roi_board_gray = cv2.cvtColor(np.array(roi_board), cv2.COLOR_BGR2GRAY)
         #p = process_img(roi_board_gray)
-        findCards(roi_board, roi_board_gray, player)
-        player.displayHand()
+        findCards(roi_board, roi_board_gray, game)
         cv2.imshow('window', roi_board)
-        
+        if cv2.waitKey(25) & 0xFF == ord('d'):
+            game.displayHand()
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-        
-def keydown():
-    while(True):
-        if msvcrt.kbhit:
-            player.displayHand()
+
 if __name__ == "__main__":
-    player = playerClass.playerHand()
-    _thread.start_new_thread(main())
-    _thread.start_new_thread(keydown())
+    game = playerClass.GameState()
+    #_thread.start_new_thread(main())
+    main()
+
     
